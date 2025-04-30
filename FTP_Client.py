f484cba305a9f2e850e48d142fc0bed556ba1b39
EthynChesn <echesnut26@cornellcollege.edu>
2025-04-29 20:53:51 -0500

    Add files via upload

--- FTP_Client.py
+++ FTP_Client.py
@@ -3,20 +3,60 @@ from threading import *
 import sys
 
 def put(filename):
+    clientSocket.send('put'.encode())
     file = open(filename,'r')
-    file = filename + ':' + file.read()
+    Char = filename[-1]
+    Counter = -1
+    while not Char == "\\":
+        Counter -= 1
+        Char = filename[Counter]
+    shortname = ''
+    for index in range(Counter+1,-1):
+        shortname += filename[index]
+    shortname += filename[-1]
+    file = file.read()
+    clientSocket.send(shortname.encode())
     clientSocket.send(file.encode())
+    print('Succesfully Uploaded ' + filename)
+
+def get(getfilename):
+    global filePath
+    Request = getfilename
+    clientSocket.send('get'.encode())
+    clientSocket.send(Request.encode())
+    filecontent = clientSocket.recv(1024).decode()
+    with open(filePath+getfilename, 'w') as Writefile:
+        Writefile.write(filecontent)
+    print(getfilename + " Downloaded succesfully")
 
 def InputListener():
     Message = input()
     if Message.startswith('put '):
         put(Message.removeprefix('put '))
+    elif Message.startswith('get '):
+        get(Message.removeprefix('get '))
+    elif Message == 'list':
+        clientSocket.send('list'.encode())
+        listlength = clientSocket.recv(1024).decode()
+        if not listlength[0].isdigit():
+            listlength = clientSocket.recv(1024).decode()
+        if len(listlength) > 2:
+            listlength = listlength[0:2]
+            if not listlength[1].isdigit():
+                listlength = listlength[0]
+
+            listlength = clientSocket.recv(1024).decode()     
+        listlength = int(listlength)
+        print('List of Files on the Server:')
+        for filelist in range(listlength):
+            file = clientSocket.recv(1024).decode()
+            print(file)
 
 
-
+filePath = r"E:\\"
 hostname = gethostname()
 IPAddr = gethostbyname(hostname)
-Server = '192.168.0.57'
+Server = '10.200.4.67'
 Port = 12345
 
 #Create socket
--- FTP_Server.py
+++ FTP_Server.py
@@ -10,15 +10,24 @@ def NewClient(clientSocket, addr):
     global clients
     clients += (clientSocket, addr[1])
     while True:
-        Incoming = clientSocket.recv(1024).decode()
-        Index = 0
-        filename = ''
-        while not Incoming[Index] == ':':
-            filename += Incoming[Index]
-            Incoming  = Incoming[Index+1:]
-            Index += 1
-        filecontent = Incoming
-        filestorage[filename] = filecontent
+        IncomingCommand = clientSocket.recv(1024).decode()
+        if IncomingCommand == 'put':
+            filename = clientSocket.recv(1024).decode()
+            filecontent = clientSocket.recv(1024).decode()
+            filestorage[filename] = filecontent
+            print(addr[0] + " Uploaded " + filename)
+        if IncomingCommand == 'get':
+            filename = clientSocket.recv(1024).decode()
+            file = filestorage[filename]
+            print(addr[0] + ' requested ' + filename)
+            clientSocket.send(file.encode())
+        if IncomingCommand == 'list':
+            storagelength = str(len(filestorage))
+            clientSocket.send(storagelength.encode())
+            for file in filestorage:
+                clientSocket.send(file.encode())
+                
+
 
 
 def main():
@@ -27,15 +36,15 @@ def main():
 
     serverSocket.bind((Host, Port))
     serverSocket.listen(5)
-
+    print('Listening for clients to connect...')
     serverMessage = 'Welcome!'
 
     while True:
         Connection, addr = serverSocket.accept()
+        print('Got connection: ' + str(addr))
+        print('Listening for clients to connect...')
         ClientThread = Thread(target=NewClient, args=(Connection,addr))
         ClientThread.start()
-        print('Got Connection', addr)
-    
         Connection.send(serverMessage.encode())
 
 if __name__ == '__main__':
