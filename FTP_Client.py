9630d3e105ce7fe814925fe9495374d8c084d8b4
EthynChesn <echesnut26@cornellcollege.edu>
2025-04-30 12:29:05 -0500

    Add files via upload

--- FTP_Client.py
+++ FTP_Client.py
@@ -3,21 +3,36 @@ from threading import *
 import sys
 
 def put(filename):
-    clientSocket.send('put'.encode())
-    file = open(filename,'r')
-    Char = filename[-1]
-    Counter = -1
-    while not Char == "\\":
-        Counter -= 1
-        Char = filename[Counter]
-    shortname = ''
-    for index in range(Counter+1,-1):
-        shortname += filename[index]
-    shortname += filename[-1]
-    file = file.read()
-    clientSocket.send(shortname.encode())
-    clientSocket.send(file.encode())
-    print('Succesfully Uploaded ' + filename)
+    try:
+        file = open(filename,'r')
+    except FileNotFoundError:
+        print('Error: Directory and/or File Not Found')
+    except PermissionError:
+        print('Error: No permission to access Directory and/or File')
+    else:
+        Char = filename[-1]
+        Counter = -1
+        Handler = False
+        while not Char == "\\":
+            Counter -= 1
+            try:
+                Char = filename[Counter]
+            except:
+                print('Error Uploading File')
+                Handler = True
+                break
+        if not Handler:
+            clientSocket.send('put'.encode())
+            shortname = ''
+            for index in range(Counter+1,-1):
+                shortname += filename[index]
+            shortname += filename[-1]
+            filecontent = file.read()
+            clientSocket.send(shortname.encode())
+            clientSocket.send(filecontent.encode())
+            print('Succesfully Uploaded ' + filename)
+            file.close()
+    
 
 def get(getfilename):
     global filePath
@@ -25,11 +40,16 @@ def get(getfilename):
     clientSocket.send('get'.encode())
     clientSocket.send(Request.encode())
     filecontent = clientSocket.recv(1024).decode()
-    with open(filePath+getfilename, 'w') as Writefile:
-        Writefile.write(filecontent)
-    print(getfilename + " Downloaded succesfully")
+    if filecontent == 'error':
+        print('Error: File Not Found')
+    else:
+        with open(filePath+getfilename, 'w') as Writefile:
+            Writefile.write(filecontent)
+        print(getfilename + " Downloaded succesfully")
+        Writefile.close()
 
 def InputListener():
+    global Close
     Message = input()
     if Message.startswith('put '):
         put(Message.removeprefix('put '))
@@ -38,25 +58,39 @@ def InputListener():
     elif Message == 'list':
         clientSocket.send('list'.encode())
         listlength = clientSocket.recv(1024).decode()
-        if not listlength[0].isdigit():
-            listlength = clientSocket.recv(1024).decode()
-        if len(listlength) > 2:
-            listlength = listlength[0:2]
-            if not listlength[1].isdigit():
-                listlength = listlength[0]
-
-            listlength = clientSocket.recv(1024).decode()     
+        clientSocket.send('True'.encode())    
         listlength = int(listlength)
-        print('List of Files on the Server:')
-        for filelist in range(listlength):
-            file = clientSocket.recv(1024).decode()
-            print(file)
+        if listlength == 0:
+            print("Error: No Files Found on Server")
+        else:
+            print('List of Files on the Server:')
+            for filelist in range(listlength):
+                file = clientSocket.recv(1024).decode()
+                print(file)
+                clientSocket.send('True'.encode())
+    elif Message.startswith('delete '):
+        clientSocket.send('delete'.encode())
+        Acknowledge = clientSocket.recv(1024).decode()
+        if Acknowledge == 'True':
+            clientSocket.send(Message.removeprefix('delete ').encode())
+        outcome = clientSocket.recv(1024).decode()
+        if outcome == 'deleted':
+            print(Message.removeprefix('delete ') + ' succesfully deleted from Server')
+        else:
+            print('Error: File Not Found on Server')
+    elif Message == 'close':
+        clientSocket.send('close'.encode())
+        Acknowledge = clientSocket.recv(1024).decode()
+        if Acknowledge == 'True':
+            clientSocket.close()
+            Close = True
 
 
-filePath = "YOURPATH (RECOMMEND ROOT OF DRIVE)"
+
+filePath = r"E:\\"
 hostname = gethostname()
 IPAddr = gethostbyname(hostname)
-Server = 'HOSTIP (IP of DEVICE HOSTING SERVER)'
+Server = '10.200.4.67'
 Port = 12345
 
 #Create socket
@@ -68,6 +102,12 @@ clientSocket.connect((Server, Port))
 #Receive and print initial information
 print(clientSocket.recv(1024).decode())
 
+Close = False
 while True:
     InputThread = Thread(target=InputListener)
     InputThread.start()
+    if Close == True:
+        break
+
+print('Disconnected from Server')
+sys.exit()
--- FTP_Server.py
+++ FTP_Server.py
@@ -1,10 +1,15 @@
 from socket import *
 from threading import *
+import time
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 clients = []
 filestorage = {}
+
+def WaitNext(clientSocket):
+    if clientSocket.recv(1024).decode() == 'True':
+        return
 
 def NewClient(clientSocket, addr):
     global clients
@@ -18,16 +23,39 @@ def NewClient(clientSocket, addr):
             print(addr[0] + " Uploaded " + filename)
         if IncomingCommand == 'get':
             filename = clientSocket.recv(1024).decode()
-            file = filestorage[filename]
             print(addr[0] + ' requested ' + filename)
-            clientSocket.send(file.encode())
-        if IncomingCommand == 'list':
-            storagelength = str(len(filestorage))
-            clientSocket.send(storagelength.encode())
-            for file in filestorage:
+            if not filename in filestorage:
+                print(addr[0] + ' Problem: ' + filename + " Not Found" )
+                clientSocket.send('error'.encode())
+            else:
+                file = filestorage[filename]
                 clientSocket.send(file.encode())
-                
-
+        if IncomingCommand == 'list':
+            storagelength = str(len(filestorage))  
+            clientSocket.send(storagelength.encode())
+            if len(filestorage) > 0:
+                Acknowledge = clientSocket.recv(1024).decode()
+                if Acknowledge == 'True':
+                    for file in filestorage:
+                        clientSocket.send(file.encode())
+                        WaitNext(clientSocket)
+                Acknowledge = False
+        if IncomingCommand == 'delete':
+            clientSocket.send('True'.encode())
+            filename = clientSocket.recv(1024).decode()
+            print(addr[0] + ' is attempting to delete ' + filename)
+            if filename in filestorage:
+                del filestorage[filename]
+                clientSocket.send('deleted'.encode())
+                print(filename + ' deleted by ' + addr[0])
+            else:
+                clientSocket.send('error'.encode())
+                print(addr[0] + ' Problem: ' + filename + ' Not Found')
+        if IncomingCommand == 'close':
+            clientSocket.send('True'.encode())
+            clientSocket.close()
+            print(addr[0] + ' has disconnected')
+            break
 
 
 def main():
