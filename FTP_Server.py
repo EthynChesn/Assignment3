8e6895969e21f9cf50ab02969805534b3d8e53c5
EthynChesn <echesnut26@cornellcollege.edu>
2025-05-02 14:47:31 -0500

    Add files via upload

--- FTP_Server.py
+++ FTP_Server.py
@@ -7,25 +7,30 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 
 clients = []
 filestorage = {}
+connections = []
 
 def WaitNext(clientSocket):
     if clientSocket.recv(1024).decode() == 'True':
         return
 
-def NewClient(clientSocket, addr, username):
+def NewClient(clientSocket, addr):
     global clients
+    global connections
+    clients += [addr[0]]
+    connections += [clientSocket]
+
     while True:
         IncomingCommand = clientSocket.recv(1024).decode()
         if IncomingCommand == 'put':
             filename = clientSocket.recv(1024).decode()
             filecontent = clientSocket.recv(1024).decode()
             filestorage[filename] = filecontent
-            print(username + " Uploaded " + filename)
+            print(addr[0] + " Uploaded " + filename)
         if IncomingCommand == 'get':
             filename = clientSocket.recv(1024).decode()
-            print(username + ' requested ' + filename)
+            print(addr[0] + ' requested ' + filename)
             if not filename in filestorage:
-                print(username + ' Problem: ' + filename + " Not Found" )
+                print(addr[0] + ' Problem: ' + filename + " Not Found" )
                 clientSocket.send('error'.encode())
             else:
                 file = filestorage[filename]
@@ -43,22 +48,22 @@ def NewClient(clientSocket, addr, username):
         if IncomingCommand == 'delete':
             clientSocket.send('True'.encode())
             filename = clientSocket.recv(1024).decode()
-            print(username + ' is attempting to delete ' + filename)
+            print(addr[0] + ' is attempting to delete ' + filename)
             if filename in filestorage:
                 del filestorage[filename]
                 clientSocket.send('deleted'.encode())
-                print(filename + ' deleted by ' + username)
+                print(filename + ' deleted by ' + addr[0])
             else:
                 clientSocket.send('error'.encode())
-                print(username + ' Problem: ' + filename + ' Not Found')
+                print(addr[0] + ' Problem: ' + filename + ' Not Found')
         if IncomingCommand == 'close':
             clientSocket.send('True'.encode())
             clientSocket.close()
-            print(username + ' has disconnected')
-            clients.remove(username)
+            print(addr[0] + ' has disconnected')
             break
 
 def InputListener():
+    global connections
     filePath = r'E:\\'
     while True:
         Command = input()
@@ -115,13 +120,18 @@ def InputListener():
             print('Files Loaded Sucessfully')
         elif Command.startswith('read '):
             filename = Command.removeprefix('read ')
-            print(filestorage[filename])    
+            print(filestorage[filename])
+        elif Command == 'close':
+            print('Server Shutting Down')
+            for connection in connections:
+                connection.close()
+            serverSocket.close()
+            sys.exit()
      
 
 
 
 def main():
-    global clients
     Host = '0.0.0.0'
     Port = 12345
 
@@ -136,15 +146,9 @@ def main():
   
     while True:
         Connection, addr = serverSocket.accept()
-        clientname = Connection.recv(1024).decode()
-        if not clientname in clients:
-            clients += [clientname]
-            Connection.send('Accept'.encode())
-        else:
-            Connection.send('Reject'.encode())
-        print('Got connection: ' + clientname + ' (' + addr[0] + ')')
+        print('Got connection: ' + str(addr))
         print('Listening for clients to connect...')
-        ClientThread = Thread(target=NewClient, args=(Connection,addr,clientname))
+        ClientThread = Thread(target=NewClient, args=(Connection,addr))
         ClientThread.start()
         Connection.send(serverMessage.encode())
 
